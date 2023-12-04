4d2dfba3ce4cd061aae0351ed936f74cd1999980
PrayagBhakar <prayag.bhakar@gmail.com>
2023-12-03 23:06:35 -0500

    remove ignored files

--- Day01_Trebuchet?!/rust/target/debug/advant-of-code_day01.d
+++
@@ -1,1 +0,0 @@
-/Users/prayagbhakar/Desktop/.wksp/advant-of-code/Day01_Trebuchet?!/rust/target/debug/advant-of-code_day01: /Users/prayagbhakar/Desktop/.wksp/advant-of-code/Day01_Trebuchet?!/rust/src/main.rs
