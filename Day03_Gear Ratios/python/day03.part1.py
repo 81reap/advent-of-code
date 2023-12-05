ed5c25ae5036c36e0e851d83f08f8b4eb0c43eef
PrayagBhakar <prayag.bhakar@gmail.com>
2023-12-05 02:47:48 -0500

    day03 part1&2 python

---
+++ Day03_Gear Ratios/python/day03.part1.py
@@ -0,0 +1,46 @@
+import re
+import numpy
+
+file = open('../input.txt')
+lines = file.readlines()
+engine_schematic = [list(line.strip()) for line in lines]
+
+not_symbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+
+# create an map of all valid engine part spots on the schematic
+width = len(engine_schematic)
+height = len(engine_schematic[0])
+include = numpy.zeros((width, height), dtype=bool)
+for i in range(0, width):
+	for j in range(0, height):
+		if engine_schematic[i][j] not in not_symbols :
+			# set the surrounding values to true as well
+			include[max(i-1, 0)][max(j-1, 0)] = True
+			include[max(i-1, 0)][j] = True
+			include[max(i-1, 0)][min(j+1, height)] = True
+			include[i][max(j-1, 0)] = True
+			include[i][j] = True
+			include[i][min(j+1, height)] = True
+			include[min(i+1, width)][max(j-1, 0)] = True
+			include[min(i+1, width)][j] = True
+			include[min(i+1, width)][min(j+1, height)] = True
+
+output = 0
+tmp_num = ""
+tmp_valid = False
+for i in range(0, width):
+	for j in range(0, height):
+		if engine_schematic[i][j] in not_symbols and engine_schematic[i][j] != "." :
+			tmp_num = tmp_num + engine_schematic[i][j]
+			tmp_valid = include[i][j] or tmp_valid
+		else :
+			if tmp_num != "" and tmp_valid:
+				output = output + int(tmp_num)
+			tmp_num = ""
+			tmp_valid = False
+	if tmp_num != "" and tmp_valid:
+		output = output + int(tmp_num)
+	tmp_num = ""
+	tmp_valid = False
+
+print(output)
---
+++ Day03_Gear Ratios/python/day03.part2.py
@@ -0,0 +1,55 @@
+import re
+import numpy
+
+file = open('../input.txt')
+lines = file.readlines()
+engine_schematic = [list(line.strip()) for line in lines]
+
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+width = len(engine_schematic)
+height = len(engine_schematic[0])
+output = 0
+
+def getNumber(i:int, j:int) -> int:
+	left = 0
+	for l in reversed(range(0, j)):
+		if engine_schematic[i][l] not in digits:
+			left = l+1
+			break
+
+	right = height
+	for r in range(j, height):
+		if engine_schematic[i][r] not in digits:
+			right = r
+			break
+
+	return int(''.join(engine_schematic[i][left:right]))
+
+for i in range(0, width):
+	for j in range(0, height):
+		if engine_schematic[i][j] == "*" :
+			part_numbers = set()
+			if engine_schematic[max(i-1, 0)][max(j-1, 0)] in digits :
+				part_numbers.add(getNumber(max(i-1, 0), max(j-1, 0)))
+			if engine_schematic[max(i-1, 0)][j] in digits :
+				part_numbers.add(getNumber(max(i-1, 0), j))
+			if engine_schematic[max(i-1, 0)][min(j+1, height)] in digits :
+				part_numbers.add(getNumber(max(i-1, 0), min(j+1, height)))
+			if engine_schematic[i][max(j-1, 0)] in digits :
+				part_numbers.add(getNumber(i, max(j-1, 0)))
+			if engine_schematic[i][j] in digits :
+				part_numbers.add(getNumber(i, j))
+			if engine_schematic[i][min(j+1, height)] in digits :
+				part_numbers.add(getNumber(i, min(j+1, height)))
+			if engine_schematic[min(i+1, width)][max(j-1, 0)] in digits :
+				part_numbers.add(getNumber(min(i+1, width), max(j-1, 0)))
+			if engine_schematic[min(i+1, width)][j] in digits :
+				part_numbers.add(getNumber(min(i+1, width), j))
+			if engine_schematic[min(i+1, width)][min(j+1, height)] in digits :
+				part_numbers.add(getNumber(min(i+1, width), min(j+1, height)))
+			
+			if len(part_numbers) == 2 :
+				output = output + (part_numbers.pop() * part_numbers.pop())
+
+print(output)
